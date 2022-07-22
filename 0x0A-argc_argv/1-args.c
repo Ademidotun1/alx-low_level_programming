c1d29ae9ff4ebab426ab261a937a282143a42e37
Balogun Adedotun <amormeee@gmail.com>
2022-07-22 06:58:33 -0700

    0x0A-argc-argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - print the number of arguments received
+ * @argc: Arg count
+ * @argv: Array of arg strings
  *
- * Return: Always 0 (Success)
+ * Return: 0 for successful exit
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-(void) argv; /*Ignore argv*/
-printf("%i\n", argc - 1);
-
-return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - Print all arguments received
+ * @argc: Number of arguments
+ * @argv: Arguments received
  *
- * Return: Always 0 (Success)
+ * Return: 0 for success
  */
-
 int main(int argc, char *argv[])
 {
-
-/*Declaring variables*/
-int count = 0;
-
-if (argc > 0)
-{
-/*WHILE - Print each arguments*/
-while (count < argc)
-{
-printf("%s\n", argv[count]);
-count++;
-}
-}
-return (0);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
 }
