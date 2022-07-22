9f841e43158331609666e3b261639f68569bae42
Balogun Adedotun <amormeee@gmail.com>
2022-07-22 06:46:21 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - Print the name of the executable file
+ * @argc: Argument count
+ * @argv: Array of argument strings
  *
- * Return: Always 0 (Success)
+ * Return: 0 for succesful exit
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void) argc;
-printf("%s\n", argv[0]);
-
-return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,24 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the multiplication of two integers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if true, 1 if false
+ * main - Multiply 2 numbers and print result
+ * @argc: Number of arguments
+ * @argv: Array of arguments recieved
+ *
+ * Return: 0 on success, 1 if not given two arguments
  */
 int main(int argc, char *argv[])
 {
-int a, b;
-if (argc == 3)
-{
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-printf("%d\n", a *b);
-return (0);
-}
-printf("Error\n");
-return (1);
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 }
